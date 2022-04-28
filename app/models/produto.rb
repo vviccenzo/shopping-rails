4b0123d36458391c0773869fdd9b6ee6ea1b994f
vinicius.padilha <vinicius.padilha@euax.com.br>
2022-04-28 10:16:07 -0300

    Adicionando novas validações

--- app/models/produto.rb
+++ app/models/produto.rb
@@ -1,5 +1,8 @@
 class Produto < ApplicationRecord
     validates :preco, presence: true
+    validates :nome, presence: true, length: { minimum: 3}
+    validates :descricao, presence: true, length: { minimum: 15}
+    validates :quantidade, presence: true, length: { minimum: 1 }
 
-    
+
 end
