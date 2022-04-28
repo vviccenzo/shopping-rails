b2035dac707cbb4d905522d58fe6695531275a48
vinicius.padilha <vinicius.padilha@euax.com.br>
2022-04-28 10:56:50 -0300

    Adicionado i18n para traduzir aplicação

--- app/models/produto.rb
+++ app/models/produto.rb
@@ -1,8 +1,15 @@
 class Produto < ApplicationRecord
     validates :preco, presence: true
     validates :nome, presence: true, length: { minimum: 3}
-    validates :descricao, presence: true, length: { minimum: 15}
+    #Se a descrição possui uma quantidade entre 5 caracteres e 200
+    validates :descricao, presence: true, length: { in: 5..200}
     validates :quantidade, presence: true, length: { minimum: 1 }
+    
+    #Exemplos de validações
 
+    # Se o campo cpf possui 11 caracteres
+    # validates :cpf, length: { is: 11 }
 
+    # Se o campo numero é um valor numérico do tipo inteiro
+    # validates :numero, numericality: { only_integer: true }
 end
--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,9 @@ Bundler.require(*Rails.groups)
 
 module Loja
   class Application < Rails::Application
+
+    #Irá traduzir o arquivo de configuração para o português
+    config.i18n.default_locale = "pt-BR"
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
